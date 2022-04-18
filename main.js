9484a5165536bd45198e9fc96288e4cea232090e
Shrief Essam <shriefessam1999@gmail.com>
2022-04-19 01:44:23 +0200

    adding erase button and edit draw function now we can keep drawing or erasing while we are holding the mouse button

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function createGrid(size){
       for(let j=0;j<size;j++){
          let cell  = document.createElement('div');
          cell.classList.add('cell');
-         let width = 960/size;
+         let width = 700/size;
          cell.style.width =`${width}px`; 
          grid.appendChild(cell); 
       }
@@ -15,9 +15,29 @@ function createGrid(size){
 function addingDrawAbility(){
    cells = document.querySelectorAll('.cell');
    cells.forEach((cell)=>{
-      cell.addEventListener('mouseover',()=>{
-         cell.classList.add('coloredCell');
+      cell.addEventListener('mouseover',function (e){
+         if(e.buttons == 1 ){
+            cell.classList.add('coloredCell');
+         }
       });
+      cell.addEventListener('click',()=>{
+            cell.classList.add('coloredCell');
+      });
+
+   })
+}
+
+function addingEraseAbility(){
+   cells = document.querySelectorAll('.cell');
+   cells.forEach((cell)=>{
+      cell.addEventListener('mouseover',function (e){
+         if(e.buttons == 1 || e.buttons == 3){
+            cell.classList.remove('coloredCell');
+         }
+      });
+      cell.addEventListener('click',()=>{
+         cell.classList.remove('coloredCell');
+   });
    })
 }
 
@@ -45,6 +65,15 @@ function gettingNewSize(){
 createGrid(16);
 addingDrawAbility()
 
+const colorButton = document.querySelector('#color');
+colorButton.addEventListener('click',()=>{
+   addingDrawAbility();
+   colorButton.classList.add('selectedButton');
+   const eraseButton = document.querySelector('#erase');
+   eraseButton.classList.remove('selectedButton');
+
+})
+
 
 const clearButton = document.querySelector('#clear');
 clearButton.addEventListener('click',()=>{
@@ -57,5 +86,13 @@ sizeButton.addEventListener('click',()=>{
    const newSize = gettingNewSize();
    createGrid(newSize);
    addingDrawAbility();
+})
+
+const eraseButton = document.querySelector('#erase');
+eraseButton.addEventListener('click',()=>{
+   addingEraseAbility();
+   eraseButton.classList.add('selectedButton');
+   const colorButton = document.querySelector('#color');
+   colorButton.classList.remove('selectedButton')
 })
 
